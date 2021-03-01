82e953cd6f59514b98399421c87109f785a14366
Jade <jadeve.jimenez@yahoo.com>
2021-02-28 20:43:16 -0800

    Set up, but need to execute tables

--- EmployeeSQL/sql_hw.sql
+++ EmployeeSQL/sql_hw.sql
@@ -1,47 +1,55 @@
---departments table first
 DROP TABLE departments;
+DROP TABLE dept_emp;
+DROP TABLE dept_manager;
+DROP TABLE employees;
+DROP TABLE salaries;
+DROP TABLE titles;
+
 CREATE TABLE departments(
 	dept_no VARCHAR,
-	dept_name VARCHAR 
+	dept_name VARCHAR,
+	PRIMARY KEY (dept_no)
 );
 SELECT * FROM departments
 
-DROP TABLE dept_emp;
 CREATE TABLE dept_emp(
 	emp_no INTEGER,
 	dept_no VARCHAR
 );
-SELECT * FROM dep_emp
+SELECT * FROM dept_emp
 
-DROP TABLE dept_manager
 CREATE TABLE dept_manager(
-	dept_no VARCHAR FOREIGN KEY,
-	emp_no INTEGER FOREIGN KEY	 
+	dept_no VARCHAR,
+	emp_no INTEGER,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY	(emp_no) REFERENCES employees(emp_no)
 );
 SELECT * FROM dept_manager
 
-DROP TABLE employees
 CREATE TABLE employees(
-	emp_no INTEGER PRIMARY KEY,
+	emp_no INTEGER,
 	emp_title_id VARCHAR,
 	birth_date DATE NOT NULL,
 	first_name VARCHAR,
 	last_name VARCHAR,
-	sex CHAR,
-	hire_date DATE NOT NULL
+	sex VARCHAR,
+	hire_date DATE NOT NULL,
+	PRIMARY KEY (emp_no)
 );
 SELECT * FROM employees
 
-DROP TABLE salaries
 CREATE TABLE salaries(
-	emp_no INTEGER FOREIGN KEY, 
-	salary INTEGER PRIMARY KEY
+	emp_no INTEGER, 
+	salary INTEGER,
+	PRIMARY KEY (salary),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 SELECT * FROM salaries
 
-DROP TABLE titles
 CREATE TABLE titles(
-	title_id VARCHAR FOREIGN KEY, 
-	title VARCHAR PRIMARY KEY
+	title_id VARCHAR, 
+	title VARCHAR,
+	PRIMARY KEY (title),
+	FOREIGN KEY (title_id) REFERENCES employees(emp_title_id)
 );
 SELECT * FROM titles
