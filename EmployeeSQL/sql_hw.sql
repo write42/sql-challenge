60783d930da7b492a7abcae69a7984b3220a0928
Jade <jadeve.jimenez@yahoo.com>
2021-03-05 15:01:45 -0800

    Done

--- EmployeeSQL/sql_hw.sql
+++ EmployeeSQL/sql_hw.sql
@@ -16,48 +16,48 @@ SELECT * FROM titles;
 
 --Queries
 
--- employees and salaries for first query
+--first query
 SELECT first_name,last_name, employees.emp_no FROM employees
 JOIN salaries ON employees.emp_no = salaries.emp_no
 
--- employees for second query
+-- second query
  SELECT first_name,last_name,hire_date
  FROM employees
  WHERE hire_date = '1986'
 
--- employees, departments,dept_manager for third query
+-- third query
 
 SELECT employees.last_name, employees.first_name, departments.dept_no,departments.dept_name,dept_manager.emp_no
 FROM employees
 JOIN dept_manager ON employees.emp_no=dept_manager.emp_no
 LEFT JOIN departments ON dept_manager.dept_no=departments.dept_no
 
--- employees and departments for fourth query
+-- fourth query
 SELECT employees.last_name, employees.first_name, departments.dept_name,dept_emp.emp_no
 FROM employees
 JOIN dept_emp ON employees.emp_no=dept_emp.emp_no
 LEFT JOIN departments ON dept_emp.dept_no=departments.dept_no
 
--- employees for fifth query 
+-- fifth query 
 SELECT first_name,last_name,sex 
 FROM employees
 WHERE (first_name='Hercules' AND last_name LIKE 'B%')
 
--- employees and departments for sixth query. filtered by sales department
+-- sixth query
 SELECT employees.last_name, employees.first_name, departments.dept_name,dept_emp.emp_no
 FROM employees
 JOIN dept_emp ON employees.emp_no=dept_emp.emp_no
 LEFT JOIN departments ON dept_emp.dept_no=departments.dept_no
 WHERE departments.dept_name='Sales'
 
--- employees and departments for seventh query. filtered by sales and dev.
+-- seventh query
 SELECT employees.last_name, employees.first_name, departments.dept_name,dept_emp.emp_no
 FROM employees
 JOIN dept_emp ON employees.emp_no=dept_emp.emp_no
 LEFT JOIN departments ON dept_emp.dept_no=departments.dept_no
 WHERE departments.dept_name='Sales'
 
--- employees for eighth query.
+-- eighth query
 SELECT last_name, COUNT(*)
 FROM employees
 GROUP BY last_name
