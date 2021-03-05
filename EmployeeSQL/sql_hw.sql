d47a3b226f51d34ba3e1704b2d7b9c27941c4964
Jade <jadeve.jimenez@yahoo.com>
2021-03-04 16:53:30 -0800

    need to re-order queries

--- EmployeeSQL/sql_hw.sql
+++ EmployeeSQL/sql_hw.sql
@@ -19,9 +19,10 @@ CREATE TABLE departments(
 	PRIMARY KEY (dept_no)
 );
 
-CREATE TABLE dept_emp(
-	emp_no INTEGER,
-	dept_no VARCHAR
+CREATE TABLE titles(
+	title_id VARCHAR, 
+	title VARCHAR,
+	PRIMARY KEY (title_id)
 );
 
 CREATE TABLE employees(
@@ -32,48 +33,74 @@ CREATE TABLE employees(
 	last_name VARCHAR,
 	sex VARCHAR,
 	hire_date DATE NOT NULL,
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id) 
+);
+
+CREATE TABLE dept_emp(
+	emp_no INTEGER,
+	dept_no VARCHAR,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	PRIMARY KEY (emp_no,dept_no)
 );
 
 CREATE TABLE dept_manager(
 	dept_no VARCHAR,
 	emp_no INTEGER,
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	FOREIGN KEY	(emp_no) REFERENCES employees(emp_no)
+	FOREIGN KEY	(emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (dept_no,emp_no)
 );
 
 CREATE TABLE salaries(
 	emp_no INTEGER, 
 	salary INTEGER,
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE titles(
-	title_id VARCHAR, 
-	title VARCHAR,
-	PRIMARY KEY (title)
-	--FOREIGN KEY (title_id) REFERENCES employees(emp_title_id)
-);
 
 -- employees and salaries for first query
 SELECT first_name,last_name, employees.emp_no FROM employees
 JOIN salaries ON employees.emp_no = salaries.emp_no
+
 -- employees for second query
  SELECT first_name,last_name,hire_date
  FROM employees
  WHERE hire_date = '1986'
 
--- employees and dept_manager for third query
+-- employees, departments,dept_manager for third query
+
+SELECT employees.last_name, employees.first_name, departments.dept_no,departments.dept_name,dept_manager.emp_no
+FROM employees
+JOIN dept_manager ON employees.emp_no=dept_manager.emp_no
+LEFT JOIN departments ON dept_manager.dept_no=departments.dept_no
 
 -- employees and departments for fourth query
+SELECT employees.last_name, employees.first_name, departments.dept_name,dept_emp.emp_no
+FROM employees
+JOIN dept_emp ON employees.emp_no=dept_emp.emp_no
+LEFT JOIN departments ON dept_emp.dept_no=departments.dept_no
 
 -- employees for fifth query 
 SELECT first_name,last_name,sex 
 FROM employees
 WHERE (first_name='Hercules' AND last_name LIKE 'B%')
+
 -- employees and departments for sixth query. filtered by sales department
+SELECT employees.last_name, employees.first_name, departments.dept_name,dept_emp.emp_no
+FROM employees
+JOIN dept_emp ON employees.emp_no=dept_emp.emp_no
+LEFT JOIN departments ON dept_emp.dept_no=departments.dept_no
+WHERE departments.dept_name='Sales'
 
 -- employees and departments for seventh query. filtered by sales and dev.
+SELECT employees.last_name, employees.first_name, departments.dept_name,dept_emp.emp_no
+FROM employees
+JOIN dept_emp ON employees.emp_no=dept_emp.emp_no
+LEFT JOIN departments ON dept_emp.dept_no=departments.dept_no
+WHERE departments.dept_name='Sales'
 
 -- employees for eighth query.
 SELECT last_name, COUNT(*)
